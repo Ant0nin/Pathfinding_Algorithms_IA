6f08e058753fbc8339d2a7d7e22892b880c2b473
steven <stev-kerjo@hotmail.fr>
2015-11-14 16:43:13 +0100

    Pile et noeud vide

--- Labyrinth_AI/Meilleurdabord.cpp
+++ Labyrinth_AI/Meilleurdabord.cpp
@@ -8,6 +8,31 @@ Meilleurdabord::Meilleurdabord(Noeud etatinitial, Operateur op) {
 
 
 
+	Pile pile = new Pile();
+
+	pile.insert(etatinitial, etatinitial.heurist);
+
+	while (pile.lentgh != 0) {
+
+		curNoeud = pile.pick();
+
+		if (curNoeud.sucesseur == 0) {//test but ?
+			return curNoeud;
+		}
+
+		else {
+			listEnfant = [op.lentgh];
+			for (var i = 0; i<op.lentgh; i++) {
+				curNoeudEnfant = sucesseur(curNoeud, op[i]);
+				if (valide(curNoeudEnfant)) {
+					//pile.insert(curNoeudEnfant,heurist(curNoeudEnfant));
+					listEnfant.push(curNoeudEnfant);
+				}
+			}
+			listEnfant.sort(triPile);
+			pile = pile.join(listEnfant);
+		}
+	}
 
 
 }
